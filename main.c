61b2a60f6bc128c5c21b5321c93b0de408d4aa46
Hasley23 <hasleyclone@gmail.com>
2018-03-23 23:26:39 +0300

    Product replaced by division

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main()
   int one, two;
   printf("Enter two integers:\n");
   scanf("%d\n%d", &one, &two);
-  printf("Sum is: %d\nDiff is: %d\n", one + two, one - two);
+  printf("Sum is: %d\nDivision is: %d\n", one + two, one / two);
   return 0;
 }
