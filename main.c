54cc3df3e76f2efe860a8328895e49b232ce59ed
Hasley23 <hasleyclone@gmail.com>
2018-03-23 22:37:02 +0300

    Product output added

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main()
   int one, two;
   printf("Enter two integers:\n");
   scanf("%d\n%d", &one, &two);
-  printf("Sum is: %d\nDiff is: %d\n", one + two, one - two);
-  return 0;
+  printf("Sum is: %d\nProduct is: %d\n", one + two, one * two);
+	return 0;
 }
