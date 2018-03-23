788c267722cc579f9772fdd773002819f23ab035
Hasley23 <hasleyclone@gmail.com>
2018-03-23 20:46:21 +0300

    main file modified in clone

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int main()
   int one, two;
   printf("Enter two integers:\n");
   scanf("%d\n%d", &one, &two);
-  printf("Sum is: %d\n", one + two);
+  printf("Sum is: %d\nDiff is: %d\n", one + two, one - two);
   return 0;
 }
